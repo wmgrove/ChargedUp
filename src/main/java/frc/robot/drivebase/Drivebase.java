11e29b45130b4fbe6e90d2398784e014ec6a4077
tk45software <tk45software@gmail.com>
2023-01-28 00:27:04 -0500

    Move in/count and deg/count computation to config Refactor drivebase motor creation details Make tank/arcade and brake/coast selectable

--- src/main/java/frc/robot/drivebase/Drivebase.java
+++ src/main/java/frc/robot/drivebase/Drivebase.java
@@ -23,6 +23,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import frc.robot.config.*;
 
@@ -52,11 +53,15 @@ public class Drivebase {
     CANSparkMax rightmotor2;
     CANSparkMax rightmotor3;
 
-    private Config config = new Config();
+    Config config = new Config();
 
     // driving
-    public double odoOrigin = 0;
-    public double dirOrigin = 0;
+    double odometerOrigin = 0;
+    double directionOrigin = 0;
+    enum BrakeModeType {BRAKE, COAST}
+    BrakeModeType motorBrakeMode = BrakeModeType.BRAKE;
+    enum DriverInputModeType {TANK, ARCADE}
+    DriverInputModeType driverInputMode = DriverInputModeType.TANK;
   
 
     // test mode //
@@ -73,6 +78,47 @@ public class Drivebase {
 //
 //  private functions for driver/operator input
 
+// tank control (individual left and right throttles)
+  private double c_leftthrottle() {
+    return control.getLeftY();
+  }
+  private double c_rightthrottle() {
+    return control.getRightY();
+  }
+
+// split arcade control (speed on left, steer on right)
+  private double c_speed() {
+    return control.getLeftY();
+  }
+  private double c_steer() {
+    return control.getRightX();
+  }
+
+// check driver input mode
+  private DriverInputModeType c_inputMode() {
+    if (control.getStartButtonPressed()) {
+      if (control.getPOV() == 90) {
+        driverInputMode = DriverInputModeType.ARCADE;
+      }
+      if (control.getPOV() == 270) {
+        driverInputMode = DriverInputModeType.TANK;
+      }
+    }
+    return driverInputMode;
+  }
+
+// check motor control break/coast mode
+  private BrakeModeType c_brakeMode() {
+    if (control.getStartButtonPressed()) {
+      if (control.getPOV() == 0) {
+        motorBrakeMode = BrakeModeType.COAST;
+      }
+      if (control.getStartButtonPressed()) {
+        motorBrakeMode = BrakeModeType.BRAKE;
+      }
+    }
+    return motorBrakeMode;
+  }
 
 //
 //   ###    ###   #####  #   #   ###   #####  #####  
@@ -83,6 +129,19 @@ public class Drivebase {
 //
 //  private functions for motor/pneumatic/servo output
 
+// set 
+  private void leftspeed(double speed) {
+    leftmotor1.set(speed);
+    leftmotor2.set(speed);
+    leftmotor3.set(speed);
+  }
+
+  private void rightspeed(double speed) {
+    rightmotor1.set(speed);
+    rightmotor2.set(speed);
+    rightmotor1.set(speed);
+
+  }
 
 //
 //   ####  #####  #   #   ####  #####  
@@ -103,46 +162,28 @@ public class Drivebase {
     return -rightmotor1.getEncoder().getPosition();
   }
 
-  // call with (false) to read the number of inches traveled since the last reset
-  // call with (true) to reset the odometer to zero
-  private double odometer(boolean reset) {
-    // inches per count is computed from robot wheel size, gearbox ratio, and encoder counts per motor revolution
-    final double inchesPerCount = (
-      config.kk_wheeldiameter /* inch */ / 1.0 /* diameter */
-    * 3.14159 /* diameter */ / 1.0 /* circumference */
-    * 1.0 /* circumference */ / config.kk_gearreduction /* motor revolutions */
-    * 1.0 /* motor revoluions */ / 1.0 /* count */
-    );
+  // read the number of inches traveled since the last reset
+  private double distance() {
     // raw robot odometer is average of left and right side raw values
     double odoRaw = (odoleft()+odoright())/2.0;
-    if (reset) {
-      odoOrigin = odoRaw;
-    }
-    return (odoRaw - odoOrigin) * inchesPerCount;
+    return (odoRaw - odometerOrigin) * config.kk_inchesPerCount;
+  }
+  // reset the odometer to zero
+  private void distanceReset() {
+    odometerOrigin = (odoleft()+odoright())/2.0;
   }
 
-  // call with (false) to read the number of degrees turned since the last reset
-  // call with (true) to reset the direction to zero
-  private double direction(boolean reset) {
+  // read the number of degrees turned since the last reset
+  private double direction() {
     // degrees per count is computed from robot wheel size, wheelbase, gearbox ratio, and encoder counts per motor revolution
-    final double degreesPerCount = (
-      config.kk_wheeldiameter /* inch */ / 1.0 /* diameter */
-    * 3.14159 /* diameter */ / 1.0 /* circumference */
-    * 1.0 /* circumference */ / config.kk_gearreduction /* motor revolutions */
-    * 1.0 /* motor revolutions */ / 1.0 /* count */
-    * 1.0 /* wheelbase */ / config.kk_wheelbase /* inch */
-    * 1.0 /* circle */ / 6.28308 /* wheelbase */
-    * 360.0 /* degree */ / 1.0 /* circle */
-    * config.kk_slipfactor /* slip factor */
-    );
     // raw robot direction is difference between left and right side raw values
     double dirRaw = (odoleft()-odoright());
-    if (reset) {
-      dirOrigin = dirRaw;
-    }
-    return (dirRaw - dirOrigin) * degreesPerCount;
+    return (dirRaw - directionOrigin) * config.kk_degreesPerCount;
   }
-
+// reset the direction to zero
+  private void directionReset() {
+    directionOrigin = (odoleft()-odoright());
+  }
 
 
 //
@@ -154,6 +195,16 @@ public class Drivebase {
 //
 //  public functions for autonomous input
 
+//
+// utility functions
+
+// create a new motor controller for a NEO brushless motor
+  private CANSparkMax newNEO(int canID, boolean isInverted) {
+    CANSparkMax motor = new CANSparkMax(canID, MotorType.kBrushless);
+    motor.setIdleMode(IdleMode.kBrake);
+    motor.setInverted(isInverted);
+    return motor;
+  }
 
 //
 //   ###    ###   #   #   ####  #####  ####   #   #   ###   #####   ###   ####
@@ -162,20 +213,18 @@ public class Drivebase {
 //  #   #  #   #  #  ##      #    #    #   #  #   #  #   #    #    #   #  #   #
 //   ###    ###   #   #  ####     #    #   #   ###    ###     #     ###   #   #
 //
-//  creates a new Subsystem 
+//  creates a new Drivebase 
 
   public Drivebase(XboxController driverControl) {
     // initialize
     control = driverControl;
-    leftmotor1 = new CANSparkMax(1, MotorType.kBrushless);
-    leftmotor2 = new CANSparkMax(3, MotorType.kBrushless);
-    leftmotor3 = new CANSparkMax(5, MotorType.kBrushless);
-    rightmotor1 = new CANSparkMax(2, MotorType.kBrushless);
-    rightmotor2 = new CANSparkMax(4, MotorType.kBrushless);
-    rightmotor3 = new CANSparkMax(6, MotorType.kBrushless);
-    rightmotor1.setInverted(true);
-    rightmotor2.setInverted(true);
-    rightmotor3.setInverted(true);
+
+    leftmotor1 = newNEO(config.kmc_left1, false);
+    leftmotor2 = newNEO(config.kmc_left2, false);
+    leftmotor3 = newNEO(config.kmc_left3, false);
+    rightmotor1 = newNEO(config.kmc_right1, true);
+    rightmotor2 = newNEO(config.kmc_right2, true);
+    rightmotor3 = newNEO(config.kmc_right3, true);
   }
 
 
@@ -188,14 +237,37 @@ public class Drivebase {
 //
 //  does everything necessary when the robot is enabled, either autonomous or teleoperated
   public void run() {
-    double left = control.getLeftY();
-    double right = control.getRightY();
-    leftmotor1.set(left);
-    leftmotor2.set(left);
-    leftmotor3.set(left);
-    rightmotor1.set(right);
-    rightmotor2.set(right);
-    rightmotor1.set(right);
+    BrakeModeType oldBrakeMode = motorBrakeMode;
+    if (oldBrakeMode != c_brakeMode()) {
+      switch (motorBrakeMode) {
+        case BRAKE:
+          leftmotor1.setIdleMode(IdleMode.kBrake);
+          leftmotor2.setIdleMode(IdleMode.kBrake);
+          leftmotor3.setIdleMode(IdleMode.kBrake);
+          break;
+        case COAST:
+          rightmotor1.setIdleMode(IdleMode.kCoast);
+          rightmotor2.setIdleMode(IdleMode.kCoast);
+          rightmotor3.setIdleMode(IdleMode.kCoast);
+        break;
+      }
+    }
+    driverInputMode = c_inputMode();
+
+    double left = 0;
+    double right = 0;
+    switch (driverInputMode) {
+      case TANK:
+        left = c_leftthrottle();
+        right = c_rightthrottle();
+        break;
+      case ARCADE:
+        left = c_speed()+c_steer();
+        right = c_speed()-c_steer();
+        break;
+    }
+    leftspeed(left);
+    rightspeed(right);
   }
 
 
@@ -208,10 +280,26 @@ public class Drivebase {
 //
 //  does everything necessary when the robot is running, either enabled or disabled
   public void idle() {
+    switch (motorBrakeMode) {
+      case BRAKE:
+        SmartDashboard.putString("brake mode", "BRAKE");
+        break;
+      case COAST:
+        SmartDashboard.putString("brake mode", "COAST");
+        break;
+    }
+    switch (driverInputMode) {
+      case TANK:
+        SmartDashboard.putString("drive mode", "TANK");
+        break;
+      case ARCADE:
+        SmartDashboard.putString("drive mode", "ARCADE");
+        break;
+    }
     SmartDashboard.putNumber("odo left raw", odoleft());
     SmartDashboard.putNumber("odo right raw", odoright());
-    SmartDashboard.putNumber("db distance", odometer(false));
-    SmartDashboard.putNumber("db direction", direction(false));
+    SmartDashboard.putNumber("db distance", distance());
+    SmartDashboard.putNumber("db direction", direction());
   }
 
 
