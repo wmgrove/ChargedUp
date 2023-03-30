06d0ce69032ff6a7331a651752aa82bfd6e3563d
Wes Grove <=>
2023-03-30 10:23:36 -0400

    Implement constructor, make singleton, add getYaw

--- src/main/java/frc/robot/Sensors.java
+++ src/main/java/frc/robot/Sensors.java
@@ -4,5 +4,21 @@ import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SPI;
 
 public class Sensors {
+
+    static Sensors self;
+
     AHRS navx = new AHRS(SPI.Port.kMXP);
+
+    public static Sensors getSensors () {
+        if (self == null) {
+            self = new Sensors();
+        }
+        return self;
+    }
+
+    private Sensors () {}
+
+    public double getYaw () {
+        return navx.getYaw();
+    }
 }
