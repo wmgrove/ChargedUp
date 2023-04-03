8468917ea11c68a112794b5b426632de974db5ef
Wes Grove <=>
2023-04-03 09:01:01 -0400

    Add Clamp and Deadband functions, add driver const

--- src/main/java/frc/robot/Utilities.java
+++ src/main/java/frc/robot/Utilities.java
@@ -16,5 +16,44 @@ public class Utilities {
 
     //Drivebase
     public static final double ACCEL = 0;
+
+    //Controllers
+    public static final int DRIVERPORT = 0;
+
+    /**
+     * Sets a limit to the minimum non-zero value of an input. Values below
+     * this limit become zero
+     * 
+     * <p>This is useful for things like joysticks where there may be excess
+     * unwanted noise around zero
+     * 
+     * @param input The value to be processed
+     * @param limit The minimum non-zero value
+     * @return Returns input when |input| > |limit| and otherwise returns 0
+     */
+    public static double deadband (double input, double limit) {
+        if (Math.abs(input) < Math.abs(limit)) {
+            input = 0;
+        }
+        return input;
+    }
+
+    /**
+     * Sets a limit to the maximum value of an input.
+     * 
+     * <p> useful as a speed limiter.
+     * @param input The value to be processed
+     * @param limit The maximum value
+     * @return Returns input when |input| < |limit|, and otherwise returns
+     * limit with the same direction as the input
+     */
+    public static double clamp (double input, double limit) {
+        if(Math.abs(input) > Math.abs(limit)) {
+            //Dividing a value by its absolute value normalizes it to 1 or -1,
+            //preserving directionality
+            input = (input/Math.abs(input)) * Math.abs(limit);
+        }
+        return input;
+    }
     
 }
