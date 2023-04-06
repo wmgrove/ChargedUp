7719241bd09f6c08f273f8f5a6711ff75788187b
Wes Grove <=>
2023-04-06 12:12:49 -0400

    Give operator gripper controls

--- src/main/java/frc/robot/Operator.java
+++ src/main/java/frc/robot/Operator.java
@@ -1,5 +1,38 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 public class Operator {
-    
+
+    static Operator self;
+
+    Joystick controller = new Joystick(0);
+
+    Gripper gripper = Gripper.getGripper();
+
+    public static Operator getSelf () {
+        if (self == null) {
+            self = new Operator();
+        }
+        return self;
+    }
+
+    private Operator () {}
+
+    public void run() {
+        if (c_intake()) {
+            gripper.intake();
+        }
+        if (c_release()) {
+            gripper.release();
+        }
+    }
+
+    private boolean c_intake () {
+        return controller.getRawButton(Utilities.kLeftNear);
+    }
+
+    private boolean c_release () {
+        return controller.getRawButton(Utilities.kLeftFar);
+    }
 }
