dd268f2ed6bbc2a67066e9934cbba0bfa2a960c7
Wes Grove <=>
2023-04-19 17:00:32 -0400

    Add operator arm controls

--- src/main/java/frc/robot/Operator.java
+++ src/main/java/frc/robot/Operator.java
@@ -9,6 +9,7 @@ public class Operator {
     Joystick controller = new Joystick(0);
 
     Gripper gripper = Gripper.getGripper();
+    Arm arm = Arm.getArm();
 
     public static Operator getSelf () {
         if (self == null) {
@@ -26,6 +27,22 @@ public class Operator {
         if (c_release()) {
             gripper.release();
         }
+        //TODO: Real values
+        if (c_slideLeft()) {
+            arm.slide(1);
+        }
+        else if (c_slideRight()) {
+            arm.slide(-1);
+        }
+
+        if (c_extend()) {
+            arm.extend(1);
+        }
+        if (c_retract()) {
+            arm.extend(-1);
+        }
+
+        arm.elevate(Utilities.deadband(-controller.getY(), 0.2));
     }
 
     private boolean c_intake () {
@@ -34,5 +51,21 @@ public class Operator {
 
     private boolean c_release () {
         return controller.getRawButton(Utilities.kLeftFar);
+    }
+
+    private boolean c_slideLeft () {
+        return controller.getRawButton(Utilities.kLeft);
+    }
+
+    private boolean c_slideRight () {
+        return controller.getRawButton(Utilities.kRight);
+    }
+
+    private boolean c_extend () {
+        return controller.getRawButton(Utilities.kUp);
+    }
+
+    private boolean c_retract () {
+        return controller.getRawButton(Utilities.kDown);
     }
 }
